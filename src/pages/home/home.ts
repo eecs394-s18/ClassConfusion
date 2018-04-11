31620bdef676fc59b95c78099b75b458505ef366
ilhamnurjadin <ilham1400@gmail.com>
2018-04-11 17:42:56 -0500

    Added class list to home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,4 +11,15 @@ export class HomePage {
 
   }
 
+  items = [
+    'EECS 348',
+    'EECS 394',
+    'EECS 330',
+    'EECS 101'
+  ];
+
+  itemSelected(item: string) {
+    console.log("Selected Item", item);
+  }
+
 }
