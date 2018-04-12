942e93e74f7987bedef8669d7a06390323bfffcc
ilhamnurjadin <ilham1400@gmail.com>
2018-04-11 19:02:54 -0500

    changed list from buttons to radio buttons

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,25 +1,55 @@
+// import { Component } from '@angular/core';
+// import { NavController } from 'ionic-angular';
+
+// @Component({
+//   selector: 'page-home',
+//   templateUrl: 'home.html'
+// })
+// export class HomePage {
+
+//   constructor(public navCtrl: NavController) {
+
+//   }
+
+//   items = [
+//     'EECS 348',
+//     'EECS 394',
+//     'EECS 330',
+//     'EECS 101'
+//   ];
+
+//   itemSelected(item: string) {
+//     console.log("Selected Item", item);
+//   }
+
+// }
+
+
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+
+import {
+  FormGroup,
+  FormControl
+
+} from '@angular/forms';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  courses;
+  courseForm;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor() {
+    this.courseForm = new FormGroup({
+      "courses": new FormControl({value: 'EECS 336', disabled: false})
+    });
   }
 
-  items = [
-    'EECS 348',
-    'EECS 394',
-    'EECS 330',
-    'EECS 101'
-  ];
-
-  itemSelected(item: string) {
-    console.log("Selected Item", item);
+  doSubmit(event) {
+    console.log('Submitting form', this.courseForm.value);
+    event.preventDefault();
   }
-
 }
