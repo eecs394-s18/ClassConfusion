0ee332227add38d288212f0dbc2dda9d63cf9e03
ilhamnurjadin <ilham1400@gmail.com>
2018-04-11 23:40:41 -0500

    trying to resolve merge conflict pt 2

--- src/pages/topics/topics.ts
+++ src/pages/topics/topics.ts
@@ -2,7 +2,10 @@ import { FirebaseProvider } from './../../providers/firebase/firebase';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { FirebaseListObservable } from 'angularfire2/database-deprecated';
+import { AlertController } from 'ionic-angular';
+import { FirebaseApp } from 'angularfire2';
 import { CourseProvider } from './../../providers/course/course';
+
 
 @Component({
   selector: 'page-topics',
@@ -12,36 +15,44 @@ import { CourseProvider } from './../../providers/course/course';
 export class TopicsPage {
   topicList: FirebaseListObservable<any[]>;
   newTopic = '';
-  currentTopic = '';
+  currentCourse = '';
 
-  constructor(public navCtrl: NavController, public firebaseProvider: FirebaseProvider, public courseService: CourseProvider) {
+  constructor(public navCtrl: NavController, public firebaseProvider: FirebaseProvider, public courseService: CourseProvider,  public alertCtrl: AlertController, private fbApp: FirebaseApp) {
     this.topicList = this.firebaseProvider.getTopics();
-    // get all topics from firebase for testing purposes:
-    // this.firebaseProvider.getTopics().subscribe(snapshots =>
-    // {
-    //   snapshots.forEach(snapshot => {
-    //       // console.log(snapshot);
-    //   });
-    // });
-
-    this.currentTopic = courseService.currentCourse;
+    this.currentCourse = courseService.currentCourse;
   }
 
   addTopic() {
-    this.firebaseProvider.getTopics().subscribe(snapshots =>
-    {
-      if (this.newTopic.length < 1) { return; }
-      let topicNames = [];
-      snapshots.forEach(snapshot => {
-          topicNames.push(snapshot.name);
-      });
-      if (topicNames.indexOf(this.newTopic) <= -1)
-      {
-        this.firebaseProvider.addTopic(this.newTopic);
+    // changing how we check if something already exists in the list
+    // apparently this still doesn't count the voting correctly?
+
+     var topicsRef = this.fbApp.database().ref('/topics/');
+
+        topicsRef.child(this.newTopic).once('value', (snapshot) => {
+          if (snapshot.exists()) {
+            this.presentAlert();
+          }
+          else {
+            this.firebaseProvider.addTopic(this.newTopic);
+          }
+        });
+
       }
-    });
-  }
- 
+
+    // this.firebaseProvider.getTopics().subscribe(snapshots =>
+    // {
+    //   if (this.newTopic.length < 1) { return; }
+    //   let topicNames = [];
+    //   snapshots.forEach(snapshot => {
+    //       topicNames.push(snapshot.name);
+    //   });
+    //   if (topicNames.indexOf(this.newTopic) <= -1)
+    //   {
+    //     this.firebaseProvider.addTopic(this.newTopic);
+    //   }
+    // });
+
+
   removeTopic(id) {
     this.firebaseProvider.removeTopic(id);
   }
@@ -56,4 +67,13 @@ export class TopicsPage {
       console.log("Remove a vote for " + topic.name);
     }
   }
+
+
+  presentAlert() {
+  let alert = this.alertCtrl.create({
+    title: 'This item is already in the list!',
+    buttons: ['Dismiss']
+  });
+  alert.present();
+}
 }
